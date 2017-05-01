58d91038413ee775bc1ed2cd9bdc19e120a496a3
Toto Lin <csl@lunlimited.net>
2017-05-01 23:22:13 +0800

    Fixed tabs and spaces

--- Azalea/Program.cs
+++ Azalea/Program.cs
@@ -17,35 +17,36 @@ namespace Azalea
 			};
 
 			app.HelpOption("-? | --help");
-            var verbose = app.Option("-v | --verbose", "Verbose output", CommandOptionType.NoValue);
-            var bind = app.Option("-l | --listen <ADDRESS>", "Listen on the specified address", CommandOptionType.SingleValue);
-            var host = app.Option("-h | --host <ADDRESS>", "Connect to the specified host", CommandOptionType.SingleValue);
-            var role = app.Argument("[role]", "Role of this instance <host|judger|client>");
+			var verbose = app.Option("-v | --verbose", "Verbose output", CommandOptionType.NoValue);
+			var bind = app.Option("-l | --listen <ADDRESS>", "Listen on the specified address", CommandOptionType.SingleValue);
+			var host = app.Option("-h | --host <ADDRESS>", "Connect to the specified host", CommandOptionType.SingleValue);
+			var role = app.Argument("[role]", "Role of this instance <host|judger|client>");
 
-            app.OnExecute(() =>
-            {
-	            if (role.Values.Count != 1)
-	            {
-                    throw new InvalidArgumentException("Unspecified role");
-                }
+			app.OnExecute(() =>
+			{
+				if (role.Values.Count != 1)
+				{
+					throw new InvalidArgumentException("Unspecified role");
+				}
 
-                var availableRoles = new List<string> { "host", "judger", "client" };
-                if (!availableRoles.Contains(role.Value)) {
-                    throw new InvalidArgumentException(String.Format("Invalid role {0}.", role.Value));
-                }
+				var availableRoles = new List<string> { "host", "judger", "client" };
+				if (!availableRoles.Contains(role.Value))
+				{
+					throw new InvalidArgumentException(String.Format("Invalid role {0}.", role.Value));
+				}
 
-                return 0;
-            });
+				return 0;
+			});
 
-            try
-            {
-                app.Execute();
-            } 
-            catch (InvalidArgumentException e) 
-            {
-                app.Out.WriteLine("InvalidArgumentException: " + e.Message);
-                app.ShowHelp();
-            }
+			try
+			{
+				app.Execute();
+			}
+			catch (InvalidArgumentException e)
+			{
+				app.Out.WriteLine("InvalidArgumentException: " + e.Message);
+				app.ShowHelp();
+			}
 
 		}
 	}
