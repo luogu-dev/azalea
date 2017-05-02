07422d5dfbf4fa970ea9ca57b77b85943ee37db9
Toto Lin <csl@lunlimited.net>
2017-05-02 14:15:49 +0800

    CommandLine; Broadcast

---
+++ Azalea/Networking/Broadcast.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+
+namespace Azalea.Networking
+{
+    public class Broadcast
+    {
+        private const int Port = 6392;
+        private IPEndPoint SendEndpoint;
+        private IPEndPoint ListenEndpoint;
+
+        public Broadcast()
+        {
+            SendEndpoint = new IPEndPoint(IPAddress.Broadcast, Port);
+            ListenEndpoint = new IPEndPoint(IPAddress.Any, Port);
+        }
+
+        public void Send() {
+            
+        }
+
+    }
+}
--- Azalea/Program.cs
+++ Azalea/Program.cs
@@ -7,47 +7,76 @@ namespace Azalea
 {
 	internal static class Program
 	{
+        private static CommandLineApplication app;
+
 		static void Main(string[] args)
 		{
-			var app = new CommandLineApplication
+			app = new CommandLineApplication
 			{
 				Name = "azalea",
 				Description = "AZALEA by Luogu",
 				FullName = "AZALEA"
 			};
 
+            SetupApp();
+
+			app.Execute(args);
+		}
+
+        static int HostCommand() 
+        {
+            
+            return 0;
+        }
+
+		static int ClientCommand()
+		{
+
+			return 0;
+		}
+
+		static int JudgerCommand()
+		{
+
+			return 0;
+		}
+
+        private static void SetupApp() {
 			app.HelpOption("-? | --help");
 			var verbose = app.Option("-v | --verbose", "Verbose output", CommandOptionType.NoValue);
-			var bind = app.Option("-l | --listen <ADDRESS>", "Listen on the specified address", CommandOptionType.SingleValue);
-			var host = app.Option("-h | --host <ADDRESS>", "Connect to the specified host", CommandOptionType.SingleValue);
-			var role = app.Argument("[role]", "Role of this instance <host|judger|client>");
-
-			app.OnExecute(() =>
+            var quiet = app.Option("-q | --quiet", "Silence output", CommandOptionType.NoValue);
+			
+			app.Command("host", config =>
 			{
-				if (role.Values.Count != 1)
-				{
-					throw new InvalidArgumentException("Unspecified role");
-				}
-
-				var availableRoles = new List<string> { "host", "judger", "client" };
-				if (!availableRoles.Contains(role.Value))
-				{
-					throw new InvalidArgumentException(String.Format("Invalid role {0}.", role.Value));
-				}
-
-				return 0;
+				config.Description = "Run this instance as host";
+                config.HelpOption("-? | --help");
+                var bind = config.Option("-l | --listen <ADDRESS>", "Listen on the specified address", CommandOptionType.SingleValue);
+                var noBroadcast = config.Option("--no-broadcast <ADDRESS>", "Do not broadcast this server", CommandOptionType.NoValue);
+                config.OnExecute(() => HostCommand());
 			});
 
-			try
+			app.Command("client", config =>
 			{
-				app.Execute();
-			}
-			catch (InvalidArgumentException e)
-			{
-				app.Out.WriteLine("InvalidArgumentException: " + e.Message);
-				app.ShowHelp();
-			}
+				config.Description = "Run this instance as client";
+                config.HelpOption("-? | --help");
+				var host = config.Option("-h | --host <ADDRESS>", "Connect to the specified host", CommandOptionType.SingleValue);
+                var noDiscovery = config.Option("--no-discovery <ADDRESS>", "Do not auto discover server", CommandOptionType.NoValue);
+				config.OnExecute(() => ClientCommand());
+			});
 
-		}
+			app.Command("judger", config =>
+			{
+				config.Description = "Run this instance as judger";
+                config.HelpOption("-? | --help");
+				var host = config.Option("-h | --host <ADDRESS>", "Connect to the specified host", CommandOptionType.SingleValue);
+                var noDiscovery = config.Option("--no-discovery <ADDRESS>", "Do not auto discover server", CommandOptionType.NoValue);
+				config.OnExecute(() => JudgerCommand());
+			});
+
+            app.OnExecute(() => {
+                app.ShowHelp();
+                return 0;
+            });
+        }
 	}
 }
